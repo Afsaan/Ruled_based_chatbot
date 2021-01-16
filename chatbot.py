286a87f53a68def4371c314ab7f4e18ae8d719a9
“Afsaan” <khanafsaan11@gmail.com>
2021-01-16 21:57:47 +0530

    added reflection

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,18 @@
 from nltk.chat.util import Chat, reflections
+
+
+print(reflections)
+
+# dummy reflection
+my_dummy_reflections= {
+    "go"     : "gone",
+    "hello"    : "hey there"
+}
+
+#default message at the start of chat
+print("Hi, I'm thecleverprogrammer and I like to chat\nPlease type lowercase English language to start a conversation. Type quit to leave ")
+#Create Chat Bot
+chat = Chat(pairs, reflections)
+
+#Start conversation
+chat.converse()
